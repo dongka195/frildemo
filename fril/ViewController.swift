da7e2939ad25a3e1cb3592a3590ce6162cb8b947
Nguyễn Đông <dongka@Nguyens-MacBook-Air.local>
2017-08-16 00:08:53 +0700

    Initial Commit

---
+++ fril/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fril
+//
+//  Created by Nguyễn Đình Đông on 8/16/17.
+//  Copyright © 2017 Nguyễn Đình Đông. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
